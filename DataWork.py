5cb584ecc36cd154e54db39e78c0cf02246c93e2
AmaduJallow <aj22011021@utg.edu.gm>
2022-08-15 02:26:59 +0000

    Yes DataWorks is cool and fun

--- DataWork.py
+++ DataWork.py
@@ -5,8 +5,10 @@ import seaborn as sn
 plt.axis([0, 50, 0, 50])
 plt.xticks(fontsize=15)
 plt.yticks(fontsize=15)
-plt.xlabel("Reservation", fontsize=20)
-plt.ylabel("Pizzas", fontsize=20)
+plt.legend()
+plt.title("Pizza Order Regression")
+plt.xlabel("Reservation", fontsize=15)
+plt.ylabel("Pizzas", fontsize=15)
 
 
 def predict(X, w):
@@ -17,28 +19,27 @@ def loss(X, Y, w):
     return np.average((predict(X, w) - Y) ** 2)
 
 
-def train(X, Y, interations, lr):
+def train(X, Y, interation, lr):
     w = 0
-    for i in range(interations):
+    for i in range(interation):
         current_loss = loss(X, Y, w)
         print("Iterations %4d => Loss: %.13f" % (i, current_loss))
         if loss(X, Y, w + lr) < current_loss:
             w += lr
-            print("width from actual value %.25f" % w)
+            # print("width from actual value %.25f" % w)
         elif loss(X, Y, w - lr) < current_loss:
             w -= lr
-            print("width from actual value %.25f" % w)
+            # print("width from actual value %.25f" % w)
         else:
-            print("width from actual value %.25f" % w)
+            # print("width from actual value %.25f" % w)
             return w
-    raise Exception("Could not converge with in %d interations" % interations)
-
+    raise Exception("Could not converge with in %d interations" % interation)
 
 
 print(plt.style.available)
 path = "C:/Users/amadu/PycharmProjects/ProjectPhoenix\Data/reser.txt"
 X, Y = np.loadtxt(path, skiprows=1, unpack=True)
-w = train(X, Y, interations=1_000_000, lr=0.01)
+w = train(X, Y, interation=1_000_000, lr=0.01)
 print("\nw=%.3f" % w)
 print("Prediction: x=%d => y=%.2f" % (20, predict(20, w)))
 sn.set()
