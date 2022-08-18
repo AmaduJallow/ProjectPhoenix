1bf69bfee355872c638479b3cfd770ae48ff5dcb
AmaduJallow <aj22011021@utg.edu.gm>
2022-08-18 16:25:33 +0000

    update

---
+++ MultiGred.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def predictor(X, w):
+    return np.matmul(X, w)
+
+
+def loss(X, Y, w):
+    return np.average((predictor(X, w) - Y) ** 2)
+
+
+def gradient(X, Y, w):
+    return 2 * np.matmul(X.T, (predictor(X, w) - Y)) / X.shape[0]
+
+
+def train(X, Y, interation, lr):
+    w = np.zeros([X.shape[1], 1])
+    for i in range(interation):
+        print("iteration %4d => Loss: %20f" % (i, loss(X, Y, w)))
+        w -= gradient(X, Y, w) * lr
+    return w
+
+
+path = "C:/Users/amadu/PycharmProjects/ProjectPhoenix/Data/pizza_3_vars.txt"
+X1, X2, X3, Y = np.loadtxt(path, skiprows=1, unpack=True)
+X = np.column_stack([X1, X2, X3])
+Y = Y.reshape(-1, 1)
+w = np.zeros([X.shape[1], 1])
+print(w)
+w = train(X, Y, interation=1_000, lr=0.001)
+
+
