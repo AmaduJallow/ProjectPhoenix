6616c9a395b6874f19fdb1e22c1485b24aa0dac6
Amadu Jallow <120021923+PhoenixAssutech@users.noreply.github.com>
2023-01-06 11:45:44 +0000

    This is it

--- scratch_01.py
+++ scratch_01.py
@@ -1,74 +1,46 @@
-import math
 from collections import namedtuple
 import matplotlib.pyplot as plt
 
+v_points = namedtuple('Point', ['x', 'y'])
 
-# Here we are using the brute force approach
-def voronoi(point_values):
-    # Initialize the data structures for the Voronoi diagram
-    edges = []
-    vertexIndices = {}
 
-    # Iterate over all pairs of points
-    for i, p1 in enumerate(point_values):
-        for j, p2 in enumerate(point_values):
-            if i == j:
+def voronoi(input_values):
+    edges_for_voronoi = []
+    for k, point_1 in enumerate(input_values):
+        for j, point_2 in enumerate(input_values):
+            if k == j or k < 0 or j < 0 or point_1.x == point_2.x or point_1.y == point_2.y:
                 continue
-            # Calculate the distance between the points
-            dx = abs(p1.x - p2.x)
-            dy = abs(p1.y - p2.y)
-            d = math.sqrt(dx ** 2 + dy ** 2)
-            # Calculate the midpoint between the points
-            x = (p1.x + p2.x) / 2
-            y = (p1.y + p2.y) / 2
-            # Check if the midpoint is within the bounds of the diagram
-            if x < 0 or y < 0:
-                continue
-            # Add the midpoint to the vertex indices dictionary
-            vertexIndices[(x, y)] = len(vertexIndices)
-            # Add the edge to the edges list
-            edges.append((d, x, y))
+            bisector_x_value = (point_1.x + point_2.x) / 2
+            bisector_y_value = (point_1.y + point_2.y) / 2
+            edges_for_voronoi.append((bisector_x_value, bisector_y_value))
 
-    return [edges, vertexIndices, point_values]
+    return edges_for_voronoi, input_values
 
 
-def generate_diagram(result):
-    voronoi_x_bisector = []
-    voronoi_y_bisector = []
-    points_x_values = []
-    points_y_values = []
+def drawer(inputs):
+    points_xs = []
+    points_ys = []
 
-    datapoints = result[0]
+    points_for_plot = inputs[0]
+    x_intercepts = []
+    y_intercepts = []
+    for k in range(len(points_for_plot)):
+        for j in range(k):
+            if points_for_plot[k][0] == points_for_plot[j][0] and points_for_plot[k][1] == points_for_plot[j][1]:
+                x_intercepts.append(points_for_plot[k][0])
+                y_intercepts.append(points_for_plot[k][1])
 
-    for i in range(len(datapoints)):
-        for j in range(i):
-            if datapoints[i][1] == datapoints[j][1] and datapoints[i][2] == datapoints[j][2]:
-                voronoi_x_bisector.append(datapoints[j][1])
-                voronoi_y_bisector.append(datapoints[j][2])
-            else:
-                continue
+    for point in inputs[1]:
+        points_xs.append(point.x)
+        points_ys.append(point.y)
 
-    for point in result[2]:
-        points_x_values.append(point.x)
-        points_y_values.append(point.y)
-
-    # Plot a graph
-    fig, axis = plt.subplots()
-    axis.set_title('Voronoi Diagram')
-    axis.set_xlabel('X Values')
-    axis.set_ylabel('Y Values')
-    axis.set_xlim(0, 4)
-    axis.set_ylim(0, 4)
-    axis.scatter(points_x_values, points_y_values, linewidth=2)
-    axis.plot(voronoi_x_bisector, voronoi_y_bisector, color='green', linestyle='dashed', marker='x',
-              markerfacecolor='blue', markersize=5)
+    fig, ax = plt.subplots()
+    ax.scatter(points_xs, points_ys)
+    ax.plot(x_intercepts, y_intercepts, linestyle='dashed', marker='x')
     plt.show()
+    return
 
 
-Point = namedtuple('Point', ['x', 'y'])
-p_0 = Point(1, 1)
-p_1 = Point(1, 2)
-p_2 = Point(2, 2)
-points = [p_0, p_1, p_2, Point(2, 1)]
+points = [v_points(3, 4), v_points(4, 3), v_points(2, 3), v_points(2, 1)]
 outputValues = voronoi(points)
-generate_diagram(outputValues)
+drawer(outputValues)
